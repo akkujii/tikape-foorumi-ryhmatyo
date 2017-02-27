12c9efc75ea8970875f1eb4844e5a8d3179b7922
eero <eero.perola@helsinki.fi>
2017-02-27 15:04:31 +0200

    Tablejen muokkausta

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -46,24 +46,25 @@ public class Database {
         lista.add("INSERT INTO Aihealue (nimi) VALUES ('Musiikki');");
         lista.add("INSERT INTO Aihealue (nimi) VALUES ('Ruuanlaitto');");
         lista.add("INSERT INTO Aihealue (nimi) VALUES ('Autot');");*/
-        lista.add("PRAGMA foreign_keys = ON; DROP TABLE Aihealue; "
+        lista.add("PRAGMA foreign_keys = ON;"
+                + "DROP TABLE Viesti;"
+                + "DROP TABLE Viestiketju;"
+                + "DROP TABLE Aihealue;"
                 + "CREATE TABLE Aihealue (aihe_id integer PRIMARY KEY, nimi varchar(20));"
                 + "INSERT INTO Aihealue (nimi) VALUES ('Urheilu');"
                 + "INSERT INTO Aihealue (nimi) VALUES ('Musiikki');"
                 + "INSERT INTO Aihealue (nimi) VALUES ('Ruuanlaitto');"
                 + "INSERT INTO Aihealue (nimi) VALUES ('Autot');");
-        lista.add("DROP TABLE Viestiketju;"
-                + "CREATE TABLE Viestiketju (ketju_id integer PRIMARY KEY, aihe_id integer, otsikko varchar(40), FOREIGN KEY(aihe_id) REFERENCES Aihealue(aihe_id));"
+        lista.add("CREATE TABLE Viestiketju (ketju_id integer PRIMARY KEY, aihe_id integer, otsikko varchar(40), FOREIGN KEY(aihe_id) REFERENCES Aihealue(aihe_id));"
                 + "INSERT INTO Viestiketju (aihe_id, otsikko) VALUES (1, 'Koris on hauskempaa kuin lätkä');"
                 + "INSERT INTO Viestiketju (aihe_id, otsikko) VALUES (1, 'Kalastus olympialajiksi');"
                 + "INSERT INTO Viestiketju (aihe_id, otsikko) VALUES (2, 'Räppi on paskaa');"
                 + "INSERT INTO Viestiketju (aihe_id, otsikko) VALUES (3, 'Nakkiperunat on parasta');"
                 + "INSERT INTO Viestiketju (aihe_id, otsikko) VALUES (4, 'Vrum vrum');");
-        lista.add("DROP TABLE Viesti;"
-                + "CREATE TABLE Viesti (viesti_id integer PRIMARY KEY, lahettaja varchar(20), lahetetty timestamp NOT NULL DEFAULT(datetime('now', '+2 hour')), viestiketju_id integer, sisalto varchar(32768), FOREIGN KEY(viestiketju_id) REFERENCES Viestiketju(ketju_id));"
-                + "INSERT INTO Viesti (lahettaja, viestiketju_id, sisalto) VALUES (Matti, 1, 'Niin on');"
-                + "INSERT INTO Viesti (lahettaja, viestiketju_id, sisalto) VALUES (Pekka, 1, 'Eipäs ole');"
-                + "INSERT INTO Viesti (lahettaja, viestiketju_id, sisalto) VALUES (Pertti, 2, 'JOOO');");
+        lista.add("CREATE TABLE Viesti (viesti_id integer PRIMARY KEY, lahettaja varchar(20), lahetetty timestamp NOT NULL DEFAULT(datetime('now', '+2 hour')), viestiketju_id integer, sisalto varchar(32768), FOREIGN KEY(viestiketju_id) REFERENCES Viestiketju(ketju_id));"
+                + "INSERT INTO Viesti (lahettaja, viestiketju_id, sisalto) VALUES ('Matti', 1, 'Niin on');"
+                + "INSERT INTO Viesti (lahettaja, viestiketju_id, sisalto) VALUES ('Pekka', 1, 'Eipäs ole');"
+                + "INSERT INTO Viesti (lahettaja, viestiketju_id, sisalto) VALUES ('Pertti', 2, 'JOOO');");
         return lista;
     }
 }
